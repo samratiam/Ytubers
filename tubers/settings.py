42e5e69098ad22816f3ec40dbae0d5dd59cef3df
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-03 16:04:50 +0545

    Add custom admin panel

--- tubers/settings.py
+++ tubers/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'djangocms_admin_style',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
