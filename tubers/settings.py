aae7055bfdfd3681ffaa4507db03144bd5b6201e
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-03 16:38:18 +0545

    Add Webpages app

--- tubers/settings.py
+++ tubers/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'webpages.apps.WebpagesConfig',
     'djangocms_admin_style',
     'django.contrib.admin',
     'django.contrib.auth',
