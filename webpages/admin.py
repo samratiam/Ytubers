02aab4404c10280c412bd898e35f74ba65f065b5
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-04 16:54:42 +0545

    Display image of team on admin panel

--- webpages/admin.py
+++ webpages/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from webpages.models import Slider, Team
+from django.utils.html import format_html
 # Register your models here.
 
 #Admin panel customisation for Team
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('id','first_name','role','created_date') #to show the fields
+    #Format HTML content to render with custom title myphoto
+    def myphoto(self, object):
+        return format_html(f'<img src = "{object.photo.url}" width="40" />')
+
+    list_display = ('id','myphoto','first_name','role','created_date') #to show the fields
     list_display_links = ('first_name','id') #to make the first_name, id clickable
     search_fields = ('first_name', 'role') #search field options
     list_filter = ('role',) #filter options by role
