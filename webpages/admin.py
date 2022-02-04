3e30ceaf61373c5b485a99f50b733e057b9659aa
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-04 16:20:47 +0545

    Register Team model

--- webpages/admin.py
+++ webpages/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from webpages.models import Slider
+from webpages.models import Slider, Team
 # Register your models here.
 
+
 admin.site.register(Slider)
+admin.site.register(Team)
