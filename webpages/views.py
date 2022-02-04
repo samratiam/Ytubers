aa4cd6053b3f38a46e01985ed0614a7bcebbe37e
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-04 17:21:55 +0545

    Pass the objects of team to home template

--- webpages/views.py
+++ webpages/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from .models import Slider
+from .models import Slider, Team
 
 # Create your views here.
 def home(request):
     sliders = Slider.objects.all()
+    teams = Team.objects.all()
     data = {
-        'sliders': sliders
+        'sliders': sliders,
+        'teams' : teams
     }
     return render(request,"webpages/home.html",data)
 
