e290812e808b1891302887b0c2b8c4651183aa45
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-05 18:46:50 +0545

    Distinct field search for city, camera type and category and pass to search template

--- youtubers/views.py
+++ youtubers/views.py
@@ -21,13 +21,35 @@ def youtubers_detail(request, id):
 def search(request):
 
     tubers = Youtuber.objects.order_by('-created_date')
+    city_search = Youtuber.objects.values_list('city',flat=True).distinct() #returns distinct array with field 'city' from model
+    camera_type_search = Youtuber.objects.values_list('camera_type',flat=True).distinct() 
+    category_search = Youtuber.objects.values_list('category',flat=True).distinct() 
 
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if keyword:
             tubers = tubers.filter(description__icontains=keyword)
 
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            tubers = tubers.filter(city__iexact=city) #exact match
+
+    if 'camera_type' in request.GET:
+        camera_type = request.GET['camera_type']
+        if camera_type:
+            tubers = tubers.filter(camera_type__iexact=camera_type) #exact match
+    
+    if 'category' in request.GET:
+        category = request.GET['category']
+        if category:
+            tubers = tubers.filter(category__iexact=category) #exact match
+        
+
     data = {
-        'tubers': tubers
+        'tubers': tubers,
+        'city_search': city_search,
+        'camera_type_search': camera_type_search,
+        'category_search': category_search,
     }
     return render(request, 'youtubers/search.html', data)
