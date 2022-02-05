45ea6ff6aa4bc1ec8746774a1c4312d4c4f5ae6b
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-05 19:24:22 +0545

    Fix urls path of accounts

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('login', views.login, name = "login"),
-    path('register', views.register, name = "register"),
-    path('logout', views.logout_user, name = "logout"), #avoid views function named "logout"
-    path('dashboard', views.dashboard, name = "dashboard"),
+    path('login/', views.login, name = "login"),
+    path('register/', views.register, name = "register"),
+    path('logout/', views.logout_user, name = "logout"), #avoid views function named "logout"
+    path('dashboard/', views.dashboard, name = "dashboard"),
 ]
