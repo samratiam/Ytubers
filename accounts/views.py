4cbbf0fca2bd734cfe47dcd9882cc53ca4faf52a
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-05 23:07:59 +0545

    Register new user after validation

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
 
 # Create your views here.
 def login(request):
     return render(request,'accounts/login.html')
 
 def register(request):
-    return render(request,'accounts/register.html')
+    if request.method == 'POST':
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
 
+        if password == confirm_password:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already exists')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(
+                            first_name = firstname, last_name = lastname, username=username, 
+                            email=email, password = password
+                           )
+                    user.save()
+                    messages.success(request, 'Account registered successfully')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Password donot match')
+            return redirect('register')
+
+    return render(request,'accounts/register.html')
 
 def logout_user(request):
     logout(request)
