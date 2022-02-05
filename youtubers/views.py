a30c34cf4aa96a4c577fcc437da71a7c08f9bdef
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-05 18:07:58 +0545

    Define keyword based query and pass the results to search template

--- youtubers/views.py
+++ youtubers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from .models import Youtuber
 
+
 def youtubers(request):
     tubers = Youtuber.objects.order_by('-created_date')
     data = {
@@ -8,7 +9,8 @@ def youtubers(request):
     }
     return render(request, 'youtubers/youtubers.html', data)
 
-def youtubers_detail(request,id):
+
+def youtubers_detail(request, id):
     tuber = get_object_or_404(Youtuber, pk=id)
     data = {
         'tuber': tuber
@@ -17,6 +19,15 @@ def youtubers_detail(request,id):
 
 
 def search(request):
-    pass
 
+    tubers = Youtuber.objects.order_by('-created_date')
 
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            tubers = tubers.filter(description__icontains=keyword)
+
+    data = {
+        'tubers': tubers
+    }
+    return render(request, 'youtubers/search.html', data)
