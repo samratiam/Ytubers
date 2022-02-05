324751e03352dc433344d0e5d5be53b42b091525
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-05 16:17:56 +0545

    Load the youtubers template page to test

--- youtubers/views.py
+++ youtubers/views.py
@@ -1,8 +1,12 @@
+from django.shortcuts import render
+
 def youtubers(request):
-    pass
+    return render(request, 'youtubers/youtubers.html')
 
 def youtubers_detail(request):
     pass
 
 def search(request):
     pass
+
+
