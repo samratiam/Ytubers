36c966b176d0efbe99664589321cd8ee657a82f1
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-05 11:40:17 +0545

    Featured youtubers pass to template home.html

--- webpages/views.py
+++ webpages/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from .models import Slider, Team
+from youtubers.models import Youtuber
 
 # Create your views here.
 def home(request):
     sliders = Slider.objects.all()
     teams = Team.objects.all()
+    featured_youtubers = Youtuber.objects.order_by('-created_date').filter(is_featured=True)
     data = {
         'sliders': sliders,
-        'teams' : teams
+        'teams' : teams,
+        'featured_youtubers': featured_youtubers,
     }
     return render(request,"webpages/home.html",data)
 
