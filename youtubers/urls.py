c8761318f5f2790ce57680b35e9b705663891ad2
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-05 16:42:55 +0545

    Update urls path of youtubers app

--- youtubers/urls.py
+++ youtubers/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('youtubers/', views.youtubers, name="youtubers"),
+    path('', views.youtubers, name="youtubers"),
     path('<int:id>/',views.youtubers_detail, name = "youtubers_detail"),
     path('search/', views.search, name="search"),
 ]
