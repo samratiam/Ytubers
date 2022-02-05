e1096ebf30c7aefd3e0fc5222468fd5639e02b14
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-05 16:41:29 +0545

    Update path of youtubers urls

--- tubers/urls.py
+++ tubers/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('webpages.urls')),
-    path('',include('youtubers.urls')),
+    path('youtubers/',include('youtubers.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
