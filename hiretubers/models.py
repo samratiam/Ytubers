26ded937b76d8463e23c230fe30e116fc7b9135f
Samrat Pudasaini <samrat.pudasaini01@gmail.com>
2022-02-06 13:46:16 +0545

    Create model for contact form

--- hiretubers/models.py
+++ hiretubers/models.py
@@ -1,3 +1,18 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
+class Hiretuber(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    tuber_id = models.IntegerField()
+    tuber_name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+    user_id = models.IntegerField(blank=True)
+    created_date = models.DateTimeField(blank=True, default=datetime.now)
+
+    def __str__(self):
+        return self.email
